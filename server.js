7c308ddd897349b6ecd76bf669900bba0bf77bdd
chigozieFrancis <francisc@hs-pforzheim.de>
2022-01-16 17:11:47 +0100

    final commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 connectDB();
 
 // Cross Access Control Enabled
-app.use(cors({origin: "https://clientcontactkeeper.netlify.app"})) 
+app.use(cors({origin: " https://contactclientkeeper.netlify.app"})) 
 
 // Init Middleware
 app.use(express.json({ extended: false }));
@@ -36,5 +36,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 /**
- * https://clientcontactkeeper.netlify.app
+ *  https://contactclientkeeper.netlify.app
  */
